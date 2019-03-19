90ec6a421d91ef88f58b8acbf7d1448d9fdd1448
Alexander Schmidt <alexander.schmidt@edvas.de>
2019-03-19 20:01:43 +0100

    add new favicon

--- index.php
+++ index.php
@@ -14,6 +14,13 @@ $active = $app->getMenu()->getActive();
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo Juri::base() . 'templates/'.$this->template; ?>/img/favicon/apple-touch-icon.png"> 
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo Juri::base() . 'templates/'.$this->template; ?>/img/favicon/favicon-32x32.png"> 
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo Juri::base() . 'templates/'.$this->template; ?>/img/favicon/favicon-16x16.png"> 
+  <link rel="manifest" href="<?php echo Juri::base() . 'templates/'.$this->template; ?>/img/favicon/site.webmanifest"> 
+  <link rel="mask-icon" href="<?php echo Juri::base() . 'templates/'.$this->template; ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5"> 
+  <meta name="msapplication-TileColor" content="#00aba9"> 
+  <meta name="theme-color" content="#ffffff">
   <style>
   @import url('https://fonts.googleapis.com/css?family=Palanquin:600|Roboto');
   </style>
